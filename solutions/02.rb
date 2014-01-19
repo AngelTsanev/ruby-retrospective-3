b833d5bec3cb230f9dd1db07c0e5856ea066ef83
Angel Tsanev <archa40@gmail.com>
2014-01-19 23:30:49 +0200

    some heavy changes

--- solutions/02.rb
+++ solutions/02.rb
@@ -1,95 +1,100 @@
 class Task
-  attr_reader :status, :description, :status, :tags
+  def self.parse(line)
+    status, description, priority, tags = line.split('|').map(&:strip)
+    tags = tags.nil? ? [] : tags.split(', ')
 
-  def initialize(arguments)
-    @status = arguments[0].downcase.to_sym
-    @description = arguments[1]
-    @priority = arguments[2].downcase.to_sym
-    @tags = (arguments[3] or "").split(', ')
+    Task.new status, description, priority, tags
   end
-  
-  def self.parse_line line
-    task_attrs = line.split('|').map do |attr|
-      attr.strip
-    end
-    Task.new *task_attrs
+
+  attr_accessor :status, :description, :priority, :tags
+
+  def initialize(status, description, priority, tags)
+    @status      = status.downcase.to_sym
+    @description = description
+    @priority    = priority.downcase.to_sym
+    @tags        = tags
   end
 end
 
-class Criteria
-  attr_accessor :proc
-
-  def initialize(proc)
-    @proc = proc
+module TodoListStatistics
+  def tasks_todo
+    @task_list.select { |task| task.status == :todo } .size
   end
 
-  class << self
-    def status(target_status)
-      new { |task| task.status == target_status }
-    end
-
-    def priority(target_priority)
-      new { |task| task.priority == target_priority }
-    end
-
-    def tags(target_tags)
-      new { |task| (target_tags - task.tags).empty? }
-    end
+  def tasks_in_progress
+    @task_list.select { |task| task.status == :current } .size
   end
 
-  def !
-    Criteria.new { |task| not @proc.call(task) }
+  def tasks_completed
+    @task_list.select { |task| task.status == :done } .size
   end
 
-  def &(other)
-    Criteria.new { |task| @proc.call(task) and other.proc.call(task) }
-  end
-
-  def |(other)
-    Criteria.new { |task| @proc.call(task) or other.proc.call(task) }
+  def completed?
+    @task_list.all? { |task| task.status == :done }
   end
 end
 
 class TodoList
-  include Enumarable
+  def self.parse(input)
+    lines = input.split("\n")
+    TodoList.new lines.map { |line| Task.parse(line) }
+  end
+
+  include Enumerable
+  include TodoListStatistics
   attr_accessor :task_list
 
-  def initialize(list)
-    @task_list = list
+  def initialize(tasks)
+    @task_list = tasks
   end
 
   def each
     @task_list.each{ |task| yield task }
   end
 
-  def self.parse(text)
-    tasks = text.each_line.map do |line|
-      Task.parse_line line
-    end
-    TodoList.new tasks
-  end
-
   def filter(criteria)
-    TodoList.new select(&criteria)
+    TodoList.new @task_list.select { |task| criteria.matches.(task) }
   end
 
   def adjoin(other)
-    TodoList.new to_a | other.to_a
+    TodoList.new (@task_list + other.task_list).uniq
+  end
+end
+
+class Criteria
+  class << self
+    def status(status)
+      Criteria.new -> (task) { task.status == status }
+    end
+
+    def priority(priority)
+      Criteria.new -> (task) { task.priority == priority }
+    end
+
+    def tags(tags)
+      Criteria.new -> (task) { (task.tags & tags).length == tags.length }
+    end
   end
 
-  def tasks_todo
-    count(&Criteria.status(:todo))
+  attr_reader :matches
+
+  def initialize(matches)
+    @matches = matches
   end
 
-  def tasks_in_progress
-    count(&Criteria.status(:current))
+  def &(other)
+    Criteria.new -> (task) do
+      matches.(task) && other.matches.(task)
+    end
   end
 
-  def tasks_completed
-    count(&Criteria.status(:done))
+  def |(other)
+    Criteria.new -> (task) do
+      matches.(task) || other.matches.(task)
+    end
   end
 
-  def completed?
-    all?(&Criteria.status(:done))
+  def !
+    Criteria.new -> (task) { not matches.(task) }
   end
 end
